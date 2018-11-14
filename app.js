e1a1867025d22a6eb2fc1c9b5ccf2f35c5adc985
Riichi Rusdiana <31721753+BillyAddlers@users.noreply.github.com>
2018-11-14 19:11:27 +0700

    Making few changes
    - Added more description on package.json
    - Added comment on app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const WebSocket = require('./structures/WebSocket.js');
 
-var frameSocket = new WebSocket(7500);
-frameSocket.registerRoots();
+var frameSocket = new WebSocket(7500); //run on port 7500
+frameSocket.registerRoots(); //register roots
