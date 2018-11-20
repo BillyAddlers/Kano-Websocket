cedbab0e5b7a5dd7df9cfc22b6ba0a794f66be30
Riichi Rusdiana <31721753+BillyAddlers@users.noreply.github.com>
2018-11-20 12:23:03 +0700

    Adding config.json

--- structures/index.js
+++ structures/index.js
@@ -2,11 +2,13 @@ const { articles } = require('./libs/getArticles');
 const { getRows, connection } = require('./libs/mysqlFullHandler');
 const { getHeader } = require('./libs/getHeader');
 const { readEngine } = require('./libs/readEngine');
+const config = require('./../config.json');
 
 module.exports = { 
     articles, 
     getRows, 
     connection, 
     getHeader,
-    readEngine
+    readEngine,
+    config
 };
